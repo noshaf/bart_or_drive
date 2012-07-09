1b1a1c4073094b1b3710588b354fc343aca92127
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-07-09 16:46:31 -0700

    added the maps file

---
+++ maps.rb
@@ -0,0 +1,43 @@
+require 'json'
+require 'net/http'
+
+module Query
+  class Maps
+    attr_reader :duration
+    def initialize(options)
+      default_options = {
+        :sensor => "false"
+      }
+      @options = default_options.merge(options)
+      @duration = trip_duration
+    end
+
+    private
+
+    def build_uri
+      URI("#{base_url}?#{url_params}")
+    end
+
+    def base_url
+      "http://maps.googleapis.com/maps/api/directions/json"
+    end
+
+    def url_params
+      @options.collect do |key, value|
+        "#{key}=#{value.gsub(' ', '+')}"
+      end.join("&")
+    end
+
+    def get_response
+      Net::HTTP.get_response(build_uri)
+    end
+
+    def trip_duration
+      json_response["routes"][0]["legs"][0]["duration"]["value"]
+    end
+
+    def json_response
+      JSON.parse(get_response.body)
+    end
+  end
+end
