754c4bc65d6960b9c0802c53dc7c20740498ce17
dbc-apprentice <apprentice@devbootcamp.com>
2012-07-10 18:17:33 -0700

    added enviornmental preference to trip logic

--- spec/trip_rspec.rb
+++ spec/trip_rspec.rb
@@ -3,12 +3,14 @@ require '../trip.rb'
 
 describe Trip do
 
-  describe '#initialize' do
+  before :each do
+     Query::Maps.any_instance.stub(:trip_duration).and_return(5)
+     @person = {}
+     @person.stub(:environmental_pref).and_return(30)
+     @trip = Trip.new('home', 'work', @person)
+  end
 
-    before :each do
-       Query::Maps.any_instance.stub(:trip_duration).and_return(5)
-       @trip = Trip.new('home', 'work')
-     end
+  describe '#initialize' do
 
     it 'should be an instance of transit trip' do
       @trip.should be_an_instance_of Trip
@@ -21,29 +23,26 @@ describe Trip do
     it 'should be initialized with a destination location' do
       @trip.destination.should eq('work')
     end
+  end
 
-    it 'initializes a driving time variable' do
-      @trip.driving_time.should eq 5
+  describe "#driving_time" do
+    it 'calculates the driving time based on environmental prefs' do
+      @trip.driving_time.should eq 35
     end
+  end
 
+  describe "#transit_time" do
     it 'initializes a transit time variable' do
       @trip.transit_time.should eq 5
     end
   end
 
+
   describe '#comparison' do
 
-    before :each do
-       Query::Maps.any_instance.stub(:trip_duration).and_return(5)
-       # Trip.any_instance.stub(:time).with('transit') { 5 } # .and_return(5)
-       # Trip.any_instance.stub(:time).with('driving') { 9 } # .and_return(9)
-       @trip = Trip.new('home', 'work')
-     end
-
     it 'compares driving time and transit time' do
-      @trip.comparison.should eq 'Driving time and transit time are equal.'
+      @trip.comparison.should match /Take Transit./
     end
-
   end
 
 end
--- trip.rb
+++ trip.rb
@@ -1,21 +1,18 @@
 require_relative './maps.rb'
 
 class Trip
-
-  attr_reader :origin, :destination, :driving_time, :transit_time
-
-  def initialize(origin, destination)
+  attr_reader :origin, :destination
+  def initialize(origin, destination, user)
     @origin = origin
     @destination = destination
-    @driving_time = time('driving')
-    @transit_time = time('transit')
+    @user = user
   end
 
   def comparison
-    if @transit_time < @driving_time
-      "Take Transit."
-    elsif @transit_time > @driving_time
-      "Drive."
+    if transit_time < driving_time
+      "Take Transit. It's #{(driving_time-transit_time)/60} minutes faster."
+    elsif transit_time > driving_time
+      "Drive. It's #{(transit_time-driving_time)/60} minutes faster."
     else
       "Driving time and transit time are equal."
     end
@@ -25,5 +22,11 @@ class Trip
     Query::Maps.new({:origin => @origin, :destination => @destination,:mode=> mode }).duration
   end
 
+  def transit_time
+    time('transit')
+  end
 
+  def driving_time
+    time('driving') + @user.environmental_pref
+  end
 end
