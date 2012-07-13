5a20294c5801d211da13879c75e85414150c0a3f
Noah Shafi + Jessie Young <apprentice+ns+jy@devbootcamp.com>
2012-07-12 20:40:31 -0700

    The commit of awesomeness

--- public/bart_or_drive.js
+++ public/bart_or_drive.js
@@ -3,8 +3,17 @@ $(document).ready( function() {
   $("#user").keyup(function (e) {
     e.preventDefault();
     $.get('/edit_user/' + $(this).val(), function(data){
-      $("#user_textbox_status").html(data);
-    })
-  });
+      $("#user_addresses").html(data);
+      $('#origin_addresses').change(function() {
+        $('#origin').val($('#origin_addresses :selected').val())
+      });
+      $('#destination_addresses').change(function() {
+        $('#destination').val($('#destination_addresses :selected').val())
+      });
+    });
+  })
+
+
   console.log("JS loaded");
 })
+
